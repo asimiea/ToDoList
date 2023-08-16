91cf6c927663bcd6a8cf2c0152cb23ea6812af69
Asimiea Sobomate-Victor <asimieasobomate@gmail.com>
2023-08-16 00:01:39 -0300

    minor changes

--- ToDoList.py
+++ ToDoList.py
@@ -1,6 +1,11 @@
-from tkinter import *
-import tkinter.messagebox
+#from tkinter import *
+#import tkinter.messagebox
+import tkinter as tk
 
-window = Tk()
+window = tk.Tk()
 window.title("TO-DO LIST APP")
+greet = tk.Label(text="This is my new To-Do list GUI")
+greet.pack()
+greeting = tk.Text()#text="This is my new To-Do list GUI")
+greeting.pack()
 window.mainloop()
