f9f48aba108c8ade67dd684d3e26433de6476da4
Asimiea Sobomate-Victor <asimieasobomate@gmail.com>
2023-08-19 21:48:14 -0300

    commit

--- ToDoList.py
+++ ToDoList.py
@@ -1,13 +1,25 @@
 #@author Sobomate-Victor Asimiea
 
-def addTask():
-    return print("adding Task")
+def addTask(entries):
+    entry = input("Enter a new task: ")
+    entries.append({"task": entry, "status": "pending"})
+    #print("adding Task")
 
-def removeTask():
-    print("marking as done")
+def removeTask(entries):
+    entry_number = int(input("Enter the number of the entry you want to mark as completed: ")) - 1
 
-def printList():
-    print("printing List")
+    if 0 <= entry_number < len(entries):
+        entries[entry_number]["status"] = "completed"
+        print("Task marked as completed!")
+    else:
+        print("Invalid entry number.")
 
-def clear():
-    print("clearing")
+def printList(entries):
+    print("TO DO List:")
+    for idx, entry in enumerate(entries, start=1):
+        print(f"{idx}. [{entry['status']}] {entry['task']}")
+    #print("printing List")
+
+def clear(entries):
+    entries.clear()
+    print("List refreshed")
--- driver.py
+++ driver.py
@@ -3,18 +3,20 @@
 import ToDoList as td
 
 class driver:
+    entries = []
+    
     while True:
-        print("What would you like to do?\n1.Add a task\n2.Mark as done\n3.View Tasks\n4.Refresh List\n5. Exit")
+        print("\nWhat would you like to do?\n1. Add a task\n2. Mark as done\n3. View List\n4. Refresh List\n5. Exit")
 
-        answer = int(input("Enter Number:\n"))
+        answer = int(input("Enter Number: "))
         if answer == 1:
-            td.addTask()
+            td.addTask(entries)
         elif answer == 2:
-            td.removeTask()
+            td.removeTask(entries)
         elif answer == 3:
-            td.printList()
+            td.printList(entries)
         elif answer == 4:
-            td.clear()
+            td.clear(entries)
         elif answer == 5:
             break
         else:
